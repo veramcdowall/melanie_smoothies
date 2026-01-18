af9f4d3c086f8a1d8d75bbbb1c2bb9f636ab1f8d
Vera McDowall <vera.mcdowall@gmail.com>
2026-01-18 18:17:40 +0000

    Update streamlit_app.py
    Expose JSON

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,4 +41,4 @@ if time_to_insert:
 # new section to display smoothiefroot nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
